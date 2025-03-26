410c1aa66a7b4f02bfd82830301502251536be84
Caio César <caio.cesar.g.souza@gmail.com>
2025-03-26 10:38:44 +0100

    remove precompiled

--- public/js/t/core.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.11.132 {:static-fns true, :optimize-constants true, :optimizations :advanced}
-goog.provide('t.core');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-if((typeof t !== 'undefined') && (typeof t.core !== 'undefined') && (typeof t.core.root !== 'undefined')){
-} else {
-t.core.root = document.getElementById("root");
-}
-(t.core.root.innerHTML = "<h1>Hello World!</h1>");
