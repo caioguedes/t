23edc51793ac39c6b27e66ac2c8e3b63a56452c5
Caio César <caio.cesar.g.souza@gmail.com>
2025-04-01 11:49:23 +0200

    Update release script to download npm deps

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,11 @@
 
 echo "Cleaning ..."
 rm -rf public/js/
+rm -rf node_modules/
 echo "Cleaning Done!"
+
+echo "Installing npm dependencies ..."
+yarn install
 
 echo "Building ..."
 clj -M:shadow-cljs --verbose release app
