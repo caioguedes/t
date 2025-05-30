5a677c4f7fe45b6119801fb893b7effcd59e080b
Caio César <caio.cesar.g.souza@gmail.com>
2025-05-30 16:11:54 +0200

    [feature] initial implementation of signin

--- src/t/core.cljs
+++ src/t/core.cljs
@@ -3,7 +3,12 @@
             [reagent.core :as r]
             [reitit.frontend.easy :as rfe]
             [reitit.frontend :as rf]
+            [ajax.core :as http]
             [cljs.pprint :refer [pprint]]))
+
+(def config {:api-url "https://dummyjson.com"})
+(defn api-url [path]
+  (str (:api-url config) path))
 
 (defonce match (r/atom nil))
 
@@ -14,31 +19,70 @@
 (defn contact-page []
   [:h1 "Contact"])
 
+(defn private-page []
+  [:h1 "Welcome"])
+
+(defn handle-signin-submit [e d]
+  (.preventDefault e)
+  (println  (http/POST
+              (api-url "/auth/login")
+              {:format :json
+               :params (select-keys d [:username :password])
+               :credentials "include"}))
+  (println d))
+
+(defn profile-page []
+  [:h1 "Profile"])
+
+(defn signin-page []
+  (let [f (r/atom {})]
+    (fn []
+      [:<>
+       [:h1 "Sign In"]
+       [:form {:method :post :on-submit #(handle-signin-submit % @f)}
+        [:label {:for :username} "Username"]
+        [:input {:type :text
+                 :name :username
+                 :required true
+                 :on-change #(reset! f (assoc @f :username (-> % .-target .-value)))}]
+        [:label {:for :password} "Password"]
+        [:input {:type :password
+                 :name :password
+                 :required true
+                 :on-change #(reset! f (assoc @f :password (-> % .-target .-value)))}]
+        [:button {:type :submit} "Sign in"]]
+       [:pre (with-out-str (pprint @f))]])))
+
 (defn notfound-page []
   [:h1 "Not Found!"])
 
 (defn navbar []
-  [:ul
-   [:li
-    [:a {:href (rfe/href :home)} "Home"]]
-   [:li
-    [:a {:href (rfe/href :contact)} "Contact"]]])
+  (let [items {:home "Home"
+               :contact "Contact"
+               :signin "Sign In"}]
+    [:ul
+     (for [item items]
+       [:li {:key (first item)}
+        [:a {:href (rfe/href (first item))} (second item)]])]))
 
 ;; App
 (defn app []
   [:<>
    [navbar]
-   (when @match
-     (let [view (:view (:data @match))]
-       [view @match]))
-   [:pre (with-out-str (cljs.pprint/pprint @match))]])
+   (if @match
+     (let [view (-> @match :data :view)]
+       [view @match])
+     [notfound-page])
+   [:pre (with-out-str (pprint @match))]])
 
 (def routes
   [["/"
     ["" {:name :home
          :view home-page}]
     ["contact" {:name :contact
-                :view contact-page}]]])
+                :view contact-page}]
+    ["singin" {:name :signin
+               :view signin-page}]]])
 
 (defonce root (rd/create-root (js/document.getElementById "root")))
 
@@ -47,5 +91,5 @@
    (rf/router routes)
    (fn [new-match]
      (reset! match new-match))
-   {:use-fragment true})
+   {:use-fragment false})
   (rd/render root [app]))
